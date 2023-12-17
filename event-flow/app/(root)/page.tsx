fe11fda4ecef0f05e0a2a69df2b428e2d530f88c
shakilshagu <muhammadshakil1968@gmail.com>
2023-12-17 20:33:32 +0530

    new commit

---
+++ event-flow/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main >
+      <h1 className='text-4xl'>Eventflow</h1>
+      <Button className='px-10'>Hello</Button>
+    </main>
+  )
+}
