cab1c1bd6f46877fa776fcb0ccc4a2fa3a150c93
shakilshagu <muhammadshakil1968@gmail.com>
2023-12-18 14:10:08 +0530

    new commit

--- event-flow/app/(root)/page.tsx
+++ event-flow/app/(root)/page.tsx
@@ -3,9 +3,18 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main >
+
       <>
+      <section className='bg-white bg-dotted-pattern bg-contain py-5 md:py-10'>
+        <div className=' wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0'>
+        <div className='flex flex-cole justify-center gap-8'>
+        <h1 className='h1-bold'>
+          Host,Connect,celebrate:your Events,our Platform!
+        </h1>
+        </div>
+        </div>
+      </section>
       </>
-    </main>
+
   )
 }
--- event-flow/components/shared/Footer.tsx
+++ event-flow/components/shared/Footer.tsx
@@ -1,8 +1,16 @@
-
+import Image from "next/image"
+import Link from "next/link"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 sm:flex-row">
+        <Link href="/">
+          <Image src="/assets/images/logo.svg" width={128} height={38} alt="footer image" />
+        </Link>
+        <p>2023 Eventflow .All Rights reserved.</p>
+      </div>
+    </footer>
   )
 }
 
