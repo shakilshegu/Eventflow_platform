13000d6a2aa1bac8aa6db7303bc690573bd99bd7
shakilshagu <muhammadshakil1968@gmail.com>
2023-12-18 11:17:45 +0530

    Login Layout

--- event-flow/components/shared/Header.tsx
+++ event-flow/components/shared/Header.tsx
@@ -1,18 +1,25 @@
+import { SignedOut } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
+import { Button } from "../ui/button"
 
 
 const Header = () => {
   return (
     <header className="w-full border-b">
       <div className="wrapper flex items-center justify-between">
-       <Link href="/" className="w-60 ">
-        <Image src="/assets/images/logo.svg" width={128} height={38} alt="Logo Images"/>
+        <Link href="/" className="w-60 ">
+          <Image src="/assets/images/logo.svg" width={128} height={38} alt="Logo Images" />
         </Link>
-      </div>
-      <div className="flex w-32 justify-end gap-3">
-        
 
+        <div className="flex w-32 justify-end gap-3">
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">Login
+              </Link>
+            </Button>
+          </SignedOut>
+        </div>
       </div>
     </header>
   )
