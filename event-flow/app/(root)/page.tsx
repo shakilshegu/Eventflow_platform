aea559a0253744e036a9e9414e24f03875ca7726
shakilshagu <muhammadshakil1968@gmail.com>
2023-12-18 19:42:18 +0530

    title

--- event-flow/app/(root)/page.tsx
+++ event-flow/app/(root)/page.tsx
@@ -26,6 +26,14 @@ export default function Home() {
           />
         </div>
       </section>
+
+      <section id="events" className='wrapper my-8 flex flex-col gap-8 md:gap-12'>
+        <h2 className='h2-bold'>Trust by<br/>Thoused of Events</h2>
+        <div className='flex w-full flex-col gap-5 md:flex-row'>
+         Search
+         CategotyFilter
+        </div>
+      </section>
     </>
 
   )
