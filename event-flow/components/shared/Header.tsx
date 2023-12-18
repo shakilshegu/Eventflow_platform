66a896e2588dd41f28e366111f7e95bf0c5d0351
shakilshagu <muhammadshakil1968@gmail.com>
2023-12-18 10:52:30 +0530

    setup clerk

--- event-flow/components/shared/Header.tsx
+++ event-flow/components/shared/Header.tsx
@@ -1,8 +1,20 @@
+import Image from "next/image"
+import Link from "next/link"
 
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+       <Link href="/" className="w-60 ">
+        <Image src="/assets/images/logo.svg" width={128} height={38} alt="Logo Images"/>
+        </Link>
+      </div>
+      <div className="flex w-32 justify-end gap-3">
+        
+
+      </div>
+    </header>
   )
 }
 
